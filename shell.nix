cc370954785a81962affd482d974ffc2a19989e2
foldu <foldu@protonmail.com>
2020-06-02 18:11:11 +0200

    Move shell stuff into .env

--- shell.nix
+++ shell.nix
@@ -8,14 +8,11 @@ pkgs.mkShell rec {
     sqlite
   ];
 
-  DB_FILE = "./data/db/db.sqlite";
-  GIT_REPO = "./data/repo";
-  DATABASE_URL = "sqlite://${DB_FILE}";
+  #DB_FILE = "./data/db/db.sqlite";
+  #GIT_REPO = "./data/repo";
+  #DATABASE_URL = "sqlite://${DB_FILE}";
 
   shellHook = ''
-    mkdir -p "$(dirname "${DB_FILE}")"
-    if ! test -f "${DB_FILE}"; then
-      sqlite3 "${DB_FILE}" -init ./schema.sql .exit
-    fi
+    source .env
   '';
 }
