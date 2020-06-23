0dd9a69218c77e0ca1c818d9773a6e4f4fd1b60e
foldu <foldu@protonmail.com>
2020-06-23 19:21:58 +0200

    Package mime.types better

--- default.nix
+++ default.nix
@@ -4,12 +4,14 @@
 , pkg-config
 , debug
 , mime-types
+, makeWrapper
 }:
 
 let
   src = builtins.filterSource
     (path: type: type != "directory" || (let basename = builtins.baseNameOf path; in basename != "target" && basename != "data"))
     ./.;
+  mimeTypesFile = "${mime-types}/etc/mime.types";
 in
 naersk.buildPackage {
   inherit src;
@@ -18,18 +20,18 @@ naersk.buildPackage {
     $src/init_db.sh
   '';
   DATABASE_URL = "sqlite://data/db/db.sqlite";
-  MIME_TYPES_PATH = "${mime-types}/etc/mime.types";
+  MIME_TYPES_PATH = mimeTypesFile;
   release = !debug;
   doCheck = debug;
   buildInputs = [
     openssl
     sqlite
     pkg-config
+    makeWrapper
   ];
   postInstall = ''
-    mkdir -p "$out/etc"
-    cp ${mime-types}/etc/mime.types "$out/etc"
     mkdir -p "$out/usr/lib/kairowiki"
     cp -r ${src}/static "$out/usr/lib/kairowiki"
+    wrapProgram "$out/bin/kairowiki" --set MIME_TYPES_PATH "${mimeTypesFile}"
   '';
 }
