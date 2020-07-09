d043d2a67c7f0ff50063364300b1787e6df96590
foldu <foldu@protonmail.com>
2020-07-09 16:55:44 +0200

    Add check constraints to db schema
    It's too much of a hassle to do proper migrations because sqlite can't
    do ALTER TABLE ADD CONSTRAINT. It's not like anybody uses this.

--- sql/file_storage_schema.sql
+++ sql/file_storage_schema.sql
@@ -1,7 +1,11 @@
 CREATE TABLE file_hash (
     id INTEGER PRIMARY KEY NOT NULL,
-    relative_path TEXT NOT NULL UNIQUE,
-    hash BLOB NOT NULL UNIQUE
+    relative_path TEXT NOT NULL UNIQUE CHECK (
+        relative_path <> ''
+    ),
+    hash BLOB NOT NULL UNIQUE CHECK (
+        LENGTH(hash) = 32
+    )
 );
 
 INSERT INTO migrations VALUES ('file_storage_schema');
