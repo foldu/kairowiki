1caf7748ac6333cd43e15ed539a6382f438a5aa4
foldu <foldu@protonmail.com>
2020-07-28 11:39:33 +0200

    Use ngrams for title search instead

--- src/index.rs
+++ src/index.rs
@@ -1,5 +1,3 @@
-mod title_segments_tokenizer;
-
 use crate::article::{ArticleTitle, WikiArticle};
 use parking_lot::Mutex;
 use std::{path::Path, time::Instant};
@@ -8,16 +6,15 @@ use tantivy::{
     directory::MmapDirectory,
     query::{QueryParser, TermQuery},
     schema::{Field, IndexRecordOption, TextFieldIndexing, TextOptions, STORED, STRING, TEXT},
-    tokenizer::{LowerCaser, TextAnalyzer},
+    tokenizer::{LowerCaser, NgramTokenizer, TextAnalyzer},
     IndexReader, IndexWriter, SnippetGenerator, TantivyError, Term,
 };
-use title_segments_tokenizer::TitleSegmentsTokenizer;
 
 #[derive(Copy, Clone)]
 pub struct Schema {
     pub title: Field,
     pub content: Field,
-    pub title_segments: Field,
+    pub title_ngrams: Field,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -55,29 +52,27 @@ impl Index {
         let dir = MmapDirectory::open(index_path).map_err(TantivyError::from)?;
 
         let mut schema = tantivy::schema::Schema::builder();
-        let title = schema.add_text_field("title", STRING);
+        let title = schema.add_text_field("title", STRING | STORED);
         let content = schema.add_text_field("content", TEXT | STORED);
 
         let text_field_indexing = TextFieldIndexing::default()
-            .set_tokenizer("title_segments")
+            .set_tokenizer("title_ngrams")
             .set_index_option(IndexRecordOption::WithFreqsAndPositions);
-        let text_options = TextOptions::default()
-            .set_indexing_options(text_field_indexing)
-            .set_stored();
-        let title_segments = schema.add_text_field("title_segments", text_options);
+        let text_options = TextOptions::default().set_indexing_options(text_field_indexing);
+        let title_ngrams = schema.add_text_field("title_ngrams", text_options);
 
         let schema = schema.build();
 
         let index = tantivy::Index::open_or_create(dir, schema)?;
         index.tokenizers().register(
-            "title_segments",
-            TextAnalyzer::from(TitleSegmentsTokenizer).filter(LowerCaser),
+            "title_ngrams",
+            TextAnalyzer::from(NgramTokenizer::prefix_only(2, 3)).filter(LowerCaser),
         );
 
         let schema = Schema {
             title,
             content,
-            title_segments,
+            title_ngrams,
         };
 
         let reader = index
@@ -108,7 +103,7 @@ impl Index {
     fn create_doc(&self, title: &ArticleTitle, content: &str) -> tantivy::Document {
         let mut doc = tantivy::Document::new();
         doc.add_text(self.schema.title, title.as_ref());
-        doc.add_text(self.schema.title_segments, title.as_ref());
+        doc.add_text(self.schema.title_ngrams, title.as_ref());
         doc.add_text(self.schema.content, &content);
         doc
     }
@@ -190,7 +185,7 @@ impl Index {
 
         let query = QueryParser::for_index(
             searcher.index(),
-            vec![self.schema.title_segments, self.schema.content],
+            vec![self.schema.title_ngrams, self.schema.content],
         )
         .parse_query(query)
         // FIXME: decide what to do if query is malformed
@@ -206,26 +201,18 @@ impl Index {
             return Ok(found);
         }
 
-        let mut title_snippet_gen =
-            SnippetGenerator::create(&searcher, &query, self.schema.title_segments)?;
-        title_snippet_gen.set_max_num_chars(std::usize::MAX);
         let content_snippet_gen = SnippetGenerator::create(&searcher, &query, self.schema.content)?;
 
         for (_score, addr) in results {
             let doc = searcher.doc(addr)?;
 
-            let title_segments = doc
-                .get_first(self.schema.title_segments)
+            let title = doc
+                .get_first(self.schema.title)
                 .unwrap()
                 .text()
-                .unwrap();
+                .unwrap()
+                .to_string();
             let content = doc.get_first(self.schema.content).unwrap().text().unwrap();
-
-            let title = String::from(title_segments);
-            let title_text = match title_snippet_gen.snippet(title_segments).to_html() {
-                snippet if snippet.len() == 0 => htmlescape::encode_minimal(&title),
-                snippet => snippet,
-            };
 
             let content_text = match content_snippet_gen.snippet(content).to_html() {
                 snippet if snippet.len() == 0 => {
@@ -238,7 +225,7 @@ impl Index {
                             match content.get(0..i) {
                                 Some(cont) => break cont,
                                 None => {
-                                    i += 1;
+                                    i = usize::min(i + 1, content.len());
                                 }
                             }
                         };
@@ -250,7 +237,6 @@ impl Index {
 
             found.push(SearchResult {
                 title,
-                title_text,
                 content_text,
             });
         }
@@ -261,6 +247,5 @@ impl Index {
 
 pub struct SearchResult {
     pub title: String,
-    pub title_text: String,
     pub content_text: String,
 }
